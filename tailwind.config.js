dfd1f484c24be481a82f049716f747c951304606
Michaël De Boey <info@michaeldeboey.be>
2021-12-10 11:05:59 +0100

    chore: update Tailwind CSS to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,13 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  mode: 'jit',
-  purge: {
-    content: ['index.html'],
-    options: {
-      keyframes: true,
-      variables: true,
-    },
-  },
-  darkMode: false, // or 'media' or 'class'
+  content: ['index.html'],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 };
