b2cc5fce288699e0440ac29f562111d9f0cb27c6
Michaël De Boey <info@michaeldeboey.be>
2022-10-06 00:32:31 +0200

    Invoice WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ['index.html', 'invoice.html', 'timesheet.html'],
   theme: {
     extend: {
       fontFamily: {
